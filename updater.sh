48aed7fa6e6b51e8164bb1ccd78dbb1aa2cb285b
Andrei Palade <andrey.palade@googlemail.com>
2015-02-08 21:08:16 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,19 +9,19 @@ DOMAIN_NAME=""
 
 #Retrieve current WAN IP
 NIP=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
-echo [$(date +"%d-%m-%Y-%T")] "CURRENT WAN IP: " $NIP  >> updates.txt
+echo [$(date +"%d-%m-%Y-%T")] "CURRENT WAN IP: " $NIP  >> log.txt
 
 #Retrieve past WAN IP
 CIP=$(curl -X GET -H 'Content-Type: application/json' -H 'Authorization: Bearer '$TOKEN'' "https://api.digitalocean.com/v2/domains/"$DOMAIN_NAME"/records/"$RECORD_ID"" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["domain_record"]["data"]')
-echo [$(date +"%d-%m-%Y-%T")] "DO WAN IP: " $CIP  >> updates.txt
+echo [$(date +"%d-%m-%Y-%T")] "DO WAN IP: " $CIP  >> log.txt
 
 
 #Compare past WAN IP and with current WAN IP
 if [ "$NIP" != "$CIP" ]; then
    curl -X PUT -H 'Content-Type: application/json' -H 'Authorization: Bearer '$TOKEN'' -d '{"data":"'"$NIP"'"}' "https://api.digitalocean.com/v2/domains/"$DOMAIN_NAME"/records/"$RECORD_ID
-   echo [$(date +"%d-%m-%Y-%T")] "UPDATING WAN IP TO: " $NIP  >> updates.txt
+   echo [$(date +"%d-%m-%Y-%T")] "UPDATING WAN IP TO: " $NIP  >> log.txt
 else
-   echo [$(date +"%d-%m-%Y-%T")] "NO CHANGES IN WAN: "$NIP" - "$CIP  >> updates.txt
+   echo [$(date +"%d-%m-%Y-%T")] "NO CHANGES IN WAN: "$NIP" - "$CIP  >> log.txt
 fi
 
 
